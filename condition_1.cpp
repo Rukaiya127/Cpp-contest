d114df76df19ae6f96240e9d37abc2dbeb808800
Rukaiya Islam Ruku <141335570+Rukaiya127@users.noreply.github.com>
2026-01-20 06:58:39 +0600

    Add files via upload

---
+++ condition_1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<stdio.h>
+#include<iomanip>
+using namespace std;
+int main(){
+    int x=5;
+    if(x==5){
+        cout << "Eaey! it is 5 "<< endl;
+    }
+   else{
+    cout << "Noo\n";
+   }
+
+return 0;
+
+}
